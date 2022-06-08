272c76cfed9e09bcb17032d21d2edbe485b0dfad
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-08 08:05:05 +0300

    commit tree a0ba557bf12c8b1dd137305792238e801187e5b7

---
+++ index.js
@@ -0,0 +1,10 @@
+//1. function expression called divide
+const divide = function () {
+  return 2000 / 100;
+};
+
+//2. arrow function called square
+const square = (root) => root * root;
+
+//3. arrow function called add
+const add = (x, y) => x + y;
